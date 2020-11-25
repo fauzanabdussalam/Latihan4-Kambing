af039527d6509dae9de0f1d8437a0c51e70ce7c5
fauzan_abdussalam <fauzan@3trust.com>
2020-11-25 23:00:00 +0700

    Initial Commit

---
+++ src/pbo10k/pkg10119901/latihan4/kambing/PBO10K10119901Latihan4Kambing.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10k.pkg10119901.latihan4.kambing;
+
+/**
+ *
+ * @author
+ * NAMA     : Fauzan Muhammad Abdussalam
+ * KELAS    : IF-10K
+ * NIM      : 10119901
+ * Deskripsi Program : Menampilkan Program Kambing
+ *
+ */
+public class PBO10K10119901Latihan4Kambing {
+
+    public void tambahKambing()
+    {
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing setelah ditambah : " + jumlahKambing);
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PBO10K10119901Latihan4Kambing kambingJantan = new PBO10K10119901Latihan4Kambing();
+        kambingJantan.tambahKambing();
+    }
+    
+}
